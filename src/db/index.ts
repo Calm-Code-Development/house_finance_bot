ef237a0092768026cc89dc2b7c5bc4217ea10a12
CamaelVentura <camael.ventura@gmail.com>
2025-11-02 23:58:41 -0300

    rollback

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,5 @@
-import * as dotenv from 'dotenv';
+import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
-
-dotenv.config();
-
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('❌ Supabase URL e KEY são obrigatórios.');
-}
 
 export const supabase = createClient(
 	process.env.SUPABASE_URL!,
