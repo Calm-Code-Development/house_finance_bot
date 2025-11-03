2baf67ab3d2069e00adec0e35013c7bdfc4b778f
CamaelVentura <camael.ventura@gmail.com>
2025-11-02 23:48:48 -0300

    add verification

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_KEY!;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('❌ Supabase URL e KEY são obrigatórios.');
+}
+
 export const supabase = createClient(
 	process.env.SUPABASE_URL!,
 	process.env.SUPABASE_KEY!,
