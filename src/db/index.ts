a20bb3f92a1868b708e4b3e3918ce7bc516c9891
CamaelVentura <camael.ventura@gmail.com>
2025-11-02 23:54:55 -0300

    improve dotenv config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,7 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_KEY!;
